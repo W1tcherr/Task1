45a2407c138ebb49515b08a508875fc95a68cb7c
W1tcherr <frost4tt@gmail.com>
2021-08-29 23:55:15 +0300

    ver 1.0

---
+++ main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+unsigned int getValue();
+unsigned int getSumm(unsigned int number);
+
+int main() {
+    std::cout << getSumm(getValue());
+    return 0;
+}
+
+unsigned int getValue()
+{
+    while(true)
+    {
+        std::cout << "Enter number: ";
+        unsigned int value;
+        std::cin >> value;
+        if(std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(32767, '\n');
+            std::cout << "\nWrong size, please re-enter.\n";
+        }
+        else
+        {
+            return value;
+        }
+    }
+}
+
+unsigned int getSumm(unsigned int number)
+{
+    if(number < 3)
+        return 0;
+    unsigned int sum = 0;
+    for(unsigned int i = 3; i <= number; ++i)
+    {
+        if(i % 3 == 0 || i % 5 == 0)
+            sum+=i;
+    }
+    return sum;
+}

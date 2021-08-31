4192cbef6d40d6398e6be6362409133829380c72
W1tcherr <frost4tt@gmail.com>
2021-08-31 21:25:19 +0300

    ver 1.1 minor fix

--- main.cpp
+++ main.cpp
@@ -30,9 +30,7 @@ unsigned int getValue()
 
 unsigned int getSumm(unsigned int number)
 {
-    if(number < 3)
-        return 0;
-    unsigned int sum = 0;
+    unsigned long long sum = 0;
     for(unsigned int i = 3; i <= number; ++i)
     {
         if(i % 3 == 0 || i % 5 == 0)
